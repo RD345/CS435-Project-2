ada588ab5d908974c56c73f4fbf23bd5857742d3
RD345 <rd345@njit.edu>
2020-03-31 15:23:58 -0400

    Created Graph class and completed questions 1 & 2.

---
+++ graph.py
@@ -0,0 +1,19 @@
+# Main file
+
+class Graph:
+
+    def __init__(self):
+        self.visited
+        self.connetions
+
+    def addNode(self):
+        print()
+        
+    def addUndirectedEdge(self):
+        print()
+
+    def removeUndirectedEdge(node1, node2):
+        print()
+
+    def getNodes(self):
+        print()
