165d8ae2f2f82fc11f65492e59d1cd7be7314d7f
Hexonoid <63165430+Hexonoid@users.noreply.github.com>
2020-04-04 18:33:31 -0400

    Re-wrote graph structure and other changes

--- graph.py
+++ graph.py
@@ -2,6 +2,7 @@
 
 from node import Node
 import random
+import node
 # Node class from my Project 1
 
 class Graph:
@@ -16,59 +17,49 @@ class Graph:
     def addNode(self, val):
         self.nodes.append(Node(str(val)))
 
-    
-    # ii.	void addUndirectedEdge(final Node first, final Node second) - This adds an undirected edge between first and second (and vice versa).
-    def addUndirectedEdge(self, node1, node2):
-        str(node1)
-        str(node2)
-
-        for node in self.nodes: # Loop through the nodes
-            if node.val == node1: # If the current node val is equal to the first node val
-                node1 = node
-            elif node.val == node2: # If the current node val is equal to the second node val
-                node2 = node
-
-        if node1 is not Node or node2 is str:
-            print("A node was not found.")
+    def getNode(self, target):
+        if target is Node:
+            return target
+        elif target is str:
+            for node in self.nodes: # Loop through the nodes
+                if node.val == target.val: # If the current node val is equal to the first node val
+                    return node
+        elif target is not Node:
+            str(target)
+            for node in self.nodes: # Loop through the nodes
+                if node.val == target: # If the current node val is equal to the first node val
+                    return node
         else:
-            node1.addEdge([node2, 0])
-            node2.addEdge([node1, 0])
+            print(target.val, "not found")
+            return None
+
+
+    # ii.	void addUndirectedEdge(final Node first, final Node second) - This adds an undirected edge between first and second (and vice versa).
+    def addUndirectedEdge(self, node1, node2, weight=None):
+        node1 = self.getNode(node1)
+        node2 = self.getNode(node2)
+        
+        node1.connections.append([node2, 0, weight])
+        node2.connections.append([node1, 0, weight])
 
     
     # iii.	void removeUndirectedEdge(final Node first, final Node second) - This removes an undirected edge between first and second (and vice versa).
     def removeUndirectedEdge(self, node1, node2):
-        node1_val = str(node1)
-        node2_val = str(node2)
+        node1 = self.getNode(node1)
+        node2 = self.getNode(node2)
 
-        for node in self.nodes: # Loop through the nodes
-            if node.val == node1_val: # If the current node is equal to the first node
-                node1 = node
-                for conn in node1.connections: # Loop thorough the connections in the first node
-                    if conn[0] == node2:
-                        node1.connections.remove(conn)
-            elif node.val == node2_val: # If the current node is equal to the second node
-                node2 = node
-                for conn in node2.connections: # Loop thorough the connections in the first node
-                    if conn[0] == node1:
-                        node1.connections.remove(conn)
+        for conn in node1.connections: # Loop thorough the connections in the first node
+            if conn[0] == node2:
+                node1.connections.remove(conn)
 
-        if node1 == str or node2 == str:
-            print("A node was not found.")
-            return
+        for conn in node2.connections: # Loop thorough the connections in the first node
+            if conn[0] == node1:
+                node1.connections.remove(conn)
         
 
     def addBiDirectionalEdge(self, node1, node2, weight=None):
-        node1_val = str(node1)
-        node2_val = str(node2)
-
-        for node in self.nodes: # Loop through the nodes
-            if node.val == node1_val: # If the current node is equal to the first node
-                node1 = node
-            elif node.val == node2_val: # If the current node is equal to the second node
-                node2 = node
-
-        if node1 == str or node2 == str:
-            print("A node was not found.")
+        if node1 == None or node2 == None:
+            print("Edge Creation failed")
             return
 
         node1.connections.append([node2, 2, weight])
@@ -111,7 +102,7 @@ class Graph:
         for node in self.nodes:
              print(node.val, end=' ')
 
-        print("\nConnections:")
+        print("\n\nConnections:")
         for node in self.nodes:
             for conn in node.connections:
                 # print(conn, end=' ')
@@ -138,12 +129,13 @@ if __name__ == "__main__":
     print("Creating Random Unweighted Graph...")
     graph = Graph()
     graph = graph.createRandomUnweightedGraphIter(20)
+    print("\nAdding an Edge...")
     graph.addUndirectedEdge("0", "1")
     graph.printGraph()
 
-    print("\nRemoving an Edge...")
-    graph.removeUndirectedEdge("0", "1")
-    graph.printGraph()
+    # print("\nRemoving an Edge...")
+    # graph.removeUndirectedEdge("0", "1")
+    # graph.printGraph()
 
     print("\nCreating LinkedList...")
     graph = graph.createLinkedList(20)
--- graphSearch.py
+++ graphSearch.py
@@ -4,15 +4,18 @@ class GraphSearch(Graph):
    
     def __init__(self):
         super().__init__()
+        self.dfs = []
 
     # TODO
     # (d)	(3 points) (You must submit code for this question!) In a class called GraphSearch, implement ArrayList<Node> DFSRec(final Node start, final Node end), which recursively returns an ArrayList of the Nodes in the Graph in a valid Depth-First Search order. The first node in the array should be start and the last should be end. If no valid DFS path goes from start to end, return null.
     def DFSRec(self, start_node, end_node):
+        start_node = self.getNode(start_node)
+        
         start_node.visited = True
+        dfs.append(start_node)
         for neighbor in start_node.connections:
             if not neighbor.visited:
-                self.DFSRec(neighbor)
-        print()
+                DFSRec(neighbor, end_node)
 
     # TODO
     # (e)	(5 points) (You must submit code for this question!) In your GraphSearch class, implement ArrayList<Node> DFSIter(final Node start, final Node end), which iteratively returns an ArrayList of the Nodes in the Graph in a valid Depth-First Search order. The first node in the array should be start and the last should be end. If no valid DFS path goes from start to end, return null.
@@ -41,6 +44,8 @@ class GraphSearch(Graph):
 
 if __name__ == "__main__":
     graph = GraphSearch()
-    graph.createRandomUnweightedGraphIter(200)
+    graph = graph.createRandomUnweightedGraphIter(200)
     graph.printGraph()
+    GraphSearch.DFSRec(graph, 0, 50)
+    print(graph.dfs)
     
